74af345959adb7fd2ae396761c4bf778abbd7174
Leonardo Celente <leocelente@gmail.com>
2021-03-06 19:08:13 -0300

    small cleanup

--- examples/serial/src/serial.cpp
+++ examples/serial/src/serial.cpp
@@ -71,55 +71,48 @@ template<int B = 255> struct SerialRead {
 	}
 };
 
-int main() {
-	using namespace attdet;
-	const Matrix3 convertMagAxis(
-	  { { 0., 1., 0. }, { 1., 0., 0. }, { 0., 0., -1. } });
 
-	std::regex data_regex(
+using namespace attdet;
+
+int main() {
+
+	const std::regex data_regex(
 	  "([\\-]?[\\d]+\\.[\\d]+),([\\-]?[\\d]+\\.[\\d]+),([\\-]?[\\d]+\\.[\\d]+),"
 	  "([\\-]?[\\d]+\\.[\\d]+),([\\-]?[\\d]+\\.[\\d]+),([\\-]?[\\d]+\\.[\\d]+),"
 	  "([\\-]?[\\d]+\\.[\\d]+),([\\-]?[\\d]+\\.[\\d]+),([\\-]?[\\d]+\\.[\\d]+)."
-	  // "([\\-]?[\\d]+\\.[\\d]+),([\\-]?[\\d]+\\.[\\d]+),([\\-]?[\\d]+\\.[\\d]+)"
 	  "\\D");
-	//!    ^
-	std::smatch s_data;
 	std::cout << std::fixed << std::setprecision(8);
 
 	const Vec3 m_ref({ -4., -18., -20. });
 	const Vec3 a_ref({ 0.16, -0.4, -9.4 });
 
-	auto mag_sensor =
-	  Sensor({ 0., 1., 0. }, alglin::normalize(convertMagAxis * m_ref), .40);
+	auto mag_sensor = Sensor({ 0., 1., 0. }, alglin::normalize(m_ref), .40);
 	auto acc_sensor = Sensor({ 0., 1., 0. }, alglin::normalize(a_ref), .60);
 
 	SerialRead<255> serial("/dev/ttyUSB0", baud::b115200);
 	while (true) {
-		std::string line = serial.readline();
+		auto line = std::string{ serial.readline() };
+		std::smatch s_data;
+
 		if (std::regex_match(line, s_data, data_regex)) {
 			std::vector<float> data{};
 			std::for_each(s_data.begin() + 1,
 			  s_data.end(),
 			  [&data](decltype(*s_data.begin()) x) {
 				  float f = std::atof(x.str().c_str());
-				  //  std::cout << f << '\t';
 				  data.push_back(f);
 			  });
+
 			acc_sensor.measure =
 			  alglin::normalize(Vec3({ data[0], data[1], data[2] }));
 			mag_sensor.measure =
-			  convertMagAxis
-			  * alglin::normalize(Vec3({ data[6], data[7], data[8] }));
+			  alglin::normalize(Vec3({ data[6], data[7], data[8] }));
 
-			// std::cout << acc_sensor.measure << mag_sensor.measure << '\n';
 			auto q = quest({ acc_sensor, mag_sensor });
-			std::cout << '\t' << acc_sensor.measure << "\t;"
-					  << mag_sensor.measure << '\n';
-			std::cout << '[' << q << "]\n";
-			std::cout << '(' << Quat2Euler(q) << ")\n";
-			// auto DCM = triad({acc_sensor, mag_sensor});
-			// std::cout << DCM;
-		}// else std::cout << "no data\n";
-		// std::cout << '(' << line << ')';
+			std::cout << "Acc: " << acc_sensor.measure;
+			std::cout << "Mag: " << mag_sensor.measure;
+			std::cout << "Quaternion: " << q;
+			std::cout << "Euler: " << Quat2Euler(q);
+		}
 	}
 }
